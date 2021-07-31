da6f2d7e4322f13b4311ac2c9438b17c1a0b52b0
abdollahiali <abdollahiali@users.noreply.github.com>
2021-07-31 00:27:18 -0700

    Draw square and circle

--- example_1.py
+++ example_1.py
@@ -11,5 +11,19 @@ class DrawCircle(Scene):
         text = Tex("Area of circle: $\pi\,r^2$").scale(1).next_to(circle, DOWN)
         self.add(text)       
         self.play(Create(text))
-        
-        
+
+class DrawSquareCircle1(Scene):
+    def construct(self):
+        square = Square(color = BLUE);
+        circle = Circle(color = GREEN).surround(square, buffer_factor = 1.03)
+        grp = Group(square, circle)
+        self.add(grp)
+
+
+class DrawSquareCircle2(Scene):
+    def construct(self):
+        square = Square(color = BLUE);
+        circle = Circle(color = GREEN).surround(square, buffer_factor = 1.03)
+        self.play(Create(square))
+        self.play(Create(circle))
+       
